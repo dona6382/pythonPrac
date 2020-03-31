71bf7de94b82ce12b21dd12656d6aa9fabb8f390
ChangHwi Kim <dona6382@epopcon.com>
2020-03-31 18:30:43 +0900

    Create Lotto.py

---
+++ example/Lotto.py
@@ -0,0 +1,14 @@
+#Lotto.py
+import random
+numberArr = list(range(1, 46))
+i = input("게임 수 : ")
+
+for j in range(0, int(i)):
+    lottoArr = []
+    while len(lottoArr) < 6:
+        num = random.choice(numberArr)
+        if not num in lottoArr:
+            lottoArr.append(num)
+    lottoArr.sort()
+    #lottoArr.sort(reverse=True) 내림차순 정렬
+    print(lottoArr) 
